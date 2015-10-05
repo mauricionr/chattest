ec8240c28403b329691da78130ce91549f9aca40
Mauricio Nunes dos Reis <mauriciotipo_0@hotmail.com>
2015-10-05 10:06:28 -0300

    index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8">
+        <title>Chattest - A Free Online Random Chat Room for Lonely Strangers!</title>
+        <meta name="description" content="Chattest is a new free online chatroom
+              and chat room community for random strangers who are bored,
+              lonely, or who just want to have a chat with new people.">
+        <meta name="keywords" content="chat, chat room, lonely, strangers, random, free online chat room">
+        <link rel="stylesheet" type="text/css" href="css/chattest_user_ops.css" media="screen" />
+        <script type="text/javascript" src="js/libraries/jquery-1.10.2.js"></script>
+        <script type="text/javascript" src="js/user_ops_main.js"></script>
+    </head>
+    <body class="body-expand body-userops">
+        <script>
+            var start_page = "<?php echo $start_page ?>";
+        </script>
+        <h1 id="header-main">Chattest</h1>
+        <div id="main-container" class="section-container-fade section-container">
+            <h2 id="header-sub">
+                An Awesome Free Online Chat Room
+            </h2>
+			</div>
+    </body>
+</html>
